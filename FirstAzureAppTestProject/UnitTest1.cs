7d9a1a446361b6a7a8979b638c9bdc6b1a6e1945
Hina-Shahzadi-08 <114351340+Hina-Shahzadi-08@users.noreply.github.com>
2023-04-03 21:22:17 +0500

    Test Cases

---
+++ FirstAzureAppTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FirstAzureAppTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
